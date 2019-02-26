19b8f18bae201d83b6e70e34bc960477b4bcb318
shivsah <sahshivam@outlook.com>
2019-02-26 17:27:59 +0530

    Merging Complete.. Program is working .. And can be found under Merged folder..

--- Merged/lyrics.java
+++ Merged/lyrics.java
@@ -2,6 +2,35 @@ import java.io.*;
 
 public class lyrics {
 
+  //from wordscount.java
+  static final int OUT = 0;
+	static final int IN = 1;
+
+  static int countWords(String str)
+	{
+		int state = OUT;
+		int wc = 0;
+		int i = 0;
+
+		while (i < str.length())
+		{
+
+			if (str.charAt(i) == ' ' || str.charAt(i) == '\n'
+					|| str.charAt(i) == '\t')
+				state = OUT;
+			else if (state == OUT)
+			{
+				state = IN;
+				++wc;
+			}
+
+			++i;
+		}
+		return wc;
+	}
+
+//******************************************************************************************************
+
 //from Main.java
 static int countOccurences(String str, String word)
 {
@@ -28,18 +57,19 @@ getCharFromString(String str, int index)
 public static void main(String[] args)
 {
     String str = "Raindrops on roses and whiskers on kittens Bright cooper kettles and warm woolen mittens Brown paper packages tied up with strings These are a few of my favorite things Cream colored ponies and crisp apple strudels Doorbells sleighbells and schnitzel with noodles Wild geese that fly with the moon on their wings These are a few of my favorite things Girls in white dresses, with blue satin sashes Snowflakes that stay on my nose and eyelashes Silver white winters that melt into springs And then I don't feel so bad";
+
     //from Main.java
     String word = "things";
     System.out.println("\n\nNo of times " +word+ " appeared is :- " +countOccurences(str, word));
+    // ***************************************************************************************************
+
+    //from charat.java
     int index = 201;
     int index1= 305;
 
 		char ch = getCharFromString(str, index);
     char ch1 = getCharFromString(str, index1);
 		System.out.println("\n\nThe character at location 201 is :- "+ ch +"\nThe character at location 305 is :- "+ch1);
-    // ***************************************************************************************************
-
-    //from charat.java
 		String[] result = str.split(" ", 2);
 		String first = result[0];
 		int size = first.length();
@@ -65,5 +95,8 @@ public static void main(String[] args)
     System.out.print("\n\n Substrings between '50 and 70' are : ");
 		System.out.println(str.substring(50, 70));
     // *****************************************************************************************************
+
+    //from wordscount.java
+    	System.out.println("\n\n Total no of words in the verse are : " + countWords(str));
 }
 }
