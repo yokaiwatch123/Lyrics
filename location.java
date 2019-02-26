4da147d2f47b2e5bbe996965db9199ac3fa3e8c9
shivsah <sahshivam@outlook.com>
2019-02-26 12:13:17 +0530

    finding dog

---
+++ location.java
@@ -0,0 +1,10 @@
+public class location {
+public static void main(String args[])
+	{
+		String Str = new String("Raindrops on roses and whiskers on kittens Bright cooper kettles and warm woolen mittens Brown paper packages tied up with strings These are a few of my favorite things Cream colored ponies and crisp apple strudels Doorbells sleighbells and schnitzel with noodles Wild geese that fly with the moon on their wings These are a few of my favorite things Girls in white dresses, with blue satin sashes Snowflakes that stay on my nose and eyelashes Silver white winters that melt into springs And then I don't feel so bad");
+
+		String subst = new String("dog");
+		System.out.print("Found dog at : ");
+		System.out.print(Str.indexOf(subst));
+	}
+}
