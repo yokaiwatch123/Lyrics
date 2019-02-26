df18093dc9df86770b6df6f343df32d6b7ac2e73
shivsah <sahshivam@outlook.com>
2019-02-26 14:25:05 +0530

    Character at location 201 & 305

---
+++ charat.java
@@ -0,0 +1,21 @@
+class charat {
+
+	public static char
+	getCharFromString(String str, int index)
+	{
+		return str.charAt(index);
+	}
+	public static void main(String[] args)
+	{
+
+		String str = "Raindrops on roses and whiskers on kittens Bright cooper kettles and warm woolen mittens Brown paper packages tied up with strings These are a few of my favorite things Cream colored ponies and crisp apple strudels Doorbells sleighbells and schnitzel with noodles Wild geese that fly with the moon on their wings These are a few of my favorite things Girls in white dresses, with blue satin sashes Snowflakes that stay on my nose and eyelashes Silver white winters that melt into springs And then I don't feel so bad";
+
+		int index = 201;
+    int index1= 305;
+
+		char ch = getCharFromString(str, index);
+    char ch1 = getCharFromString(str, index1);
+
+		System.out.println("The character at location 201 is :- "+ ch +"\nand at location 305 it's :- "+ch1);
+	}
+}
